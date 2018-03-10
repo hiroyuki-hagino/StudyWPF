8c60da5bc28a33e8050e98521bd37e11fba0ede1
hiroyuki-hagino <hiro_mogul@me.com>
2018-03-10 12:07:27 +0900

    branch 0310-3 test2
    branch 0310-3 test1
    
    branch 0310-2 test2
    
    branch 0310-2 test1

--- StudyWPF/MainWindow.xaml.cs
+++ StudyWPF/MainWindow.xaml.cs
@@ -38,6 +38,10 @@ namespace StudyWPF
             // branch test3
             // branch test4
             // branch test5
+            // branch 0310-2 test1
+            // branch 0310-2 test2
+            // branch 0310-3 test1
+            // branch 0310-3 test2
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
