cd6e6abf623fd7e5a001b98df8dd80dd702f0535
hiroyuki-hagino <hiro_mogul@me.com>
2019-06-15 15:04:11 +0900

    FriendlyDemo Add UITest

---
+++ StudyWPFUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StudyWPFUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudyWPFUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a20b4f4f-30fb-4af6-b4d5-bedfac9bef63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StudyWPFUITest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudyWPFUITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Process process;
+        private WindowsAppFriend app;
+        private MainWindowDriver driver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.process = Process.Start(@"StudyWPF.exe");
+            this.app = new WindowsAppFriend(this.process);
+            this.driver = new MainWindowDriver(app.Type("System.Windows.Application").Current.MainWindow);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.app.Dispose();
+            this.process.Kill();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            this.driver.Lhs.EmulateChangeText("10");
+            this.driver.Rhs.EmulateChangeText("3");
+            this.driver.OperatorType.EmulateChangeSelectedIndex(0); // Add
+            this.driver.Execute.EmulateClick();
+            Assert.AreEqual("13", this.driver.Answer.Text);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            this.driver.Lhs.EmulateChangeText("aaa");
+            this.driver.Rhs.EmulateChangeText("3");
+            this.driver.OperatorType.EmulateChangeSelectedIndex(0); // Add
+            this.driver.Execute.EmulateClick();
+            Assert.AreEqual("0", this.driver.Answer.Text);
+        }
+
+    }
+}
