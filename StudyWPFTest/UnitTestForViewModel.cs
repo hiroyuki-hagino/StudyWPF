292e6691d4f4a3585232588ca0ec1fe4969499f3
hiroyuki-hagino <hiro_mogul@me.com>
2019-06-22 08:50:41 +0900

    ファイル追加漏れ

---
+++ StudyWPFTest/UnitTestForViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyWPF.ViewModels;
+
+namespace StudyWPFTest
+{
+    [TestClass]
+    public class UnitTestForViewModel
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            var vm = new MainWindowViewModel();
+            vm.Lhs = "10";
+            vm.Rhs = "3";
+            vm.SelectedOperatorType = vm.OperatorTypes[0]; // Add
+
+            var command = vm.ExecuteCommand;
+            Assert.AreEqual(true, command.CanExecute());
+            command.Execute();
+            Assert.AreEqual(13, vm.Answer);
+        }
+
+        // 以下同様に...
+    }
+}
