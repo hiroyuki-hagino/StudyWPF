a12c0f2fa1991f14015088d5a7a3fa5772ed63c3
hiroyuki-hagino <hiro_mogul@me.com>
2019-08-04 08:26:10 +0900

    Add StudyPrism

---
+++ QRCodeReader/QRCodeReaderModule.cs
@@ -0,0 +1,21 @@
+﻿using QRCodeReader.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace QRCodeReader
+{
+    public class QRCodeReaderModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
