cc613f0cadf5549e86262e12268a2fd506b5abe8
hiroyuki-hagino <hiro_mogul@me.com>
2020-03-28 08:59:41 +0900

    コードレビュー指摘修正

--- QRCodeReader/ViewModels/ViewAViewModel.cs
+++ QRCodeReader/ViewModels/ViewAViewModel.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace QRCodeReader.ViewModels
 {
     /// <summary>
-    /// 
+    /// ViewA ViewModel
     /// </summary>
     public class ViewAViewModel : BindableBase
     {
